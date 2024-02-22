a235d947db66b5e4c1af8f0fbc89e8b89b729e0b
Alain Chea <alain.chea@efrei.net>
2024-02-22 16:44:30 +0100

    refactor(components): navbar

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -3,34 +3,38 @@ import styles from "./NavBar.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "@/components/Button/Button";
+import sgLogo from "@/assets/images/sgLogo.png";
+// import logo from "@/assets/vectors/logo.svg";
 
 const NavBar = () => {
   return (
-    <header className={styles.container}>
-      <Image src="/logo.svg" alt="logo" width={100} height={100} />
-      <div className={styles.listNav}>
-        <Link href={"/Accueil"} className={styles.link}>
-          Accueil
-        </Link>
-        <Link href={"/services"} className={styles.link}>
-          Nos Services
-        </Link>
-        <Link href={"/tarifs"} className={styles.link}>
-          Tarifs
-        </Link>
-        <Link href={"/Qui sommes nous ?"} className={styles.link}>
-          Qui somme nous ?
-        </Link>
+    <nav className={styles.container}>
+      <div className={styles.leftContainer}>
+        <Image src={sgLogo} alt="logo" width={100} height={100} />
+        <div className={styles.listNav}>
+          <Link href={"/Accueil"} className={styles.link}>
+            Accueil
+          </Link>
+          <Link href={"/services"} className={styles.link}>
+            Nos Services
+          </Link>
+          <Link href={"/tarifs"} className={styles.link}>
+            Tarifs
+          </Link>
+          <Link href={"/Qui sommes nous ?"} className={styles.link}>
+            Qui somme nous ?
+          </Link>
+        </div>
       </div>
       <div className={styles.buttonContainer}>
-        <Button size="lg" color="secondary">
-          Nous Contacter
-        </Button>
-        <Button size="lg" color="primary">
+        <Button size="lg" backgroundColor="primary" radius="rounded">
           Estimer votre coût
         </Button>
+        <Button size="lg" radius="rounded" backgroundColor="secondary">
+          Nous Contacter
+        </Button>
       </div>
-    </header>
+    </nav>
   );
 };
 
