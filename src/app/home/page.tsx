f7d414aafbfa2b468f230390dc28c69f1926c0a2
Alain Chea <alain.chea@efrei.net>
2024-02-24 23:25:36 +0100

    refactor(components): some styles

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,6 +2,7 @@ import styles from "./page.module.scss";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import ServicesSection from "@/components/ServicesSection/ServicesSection";
 import ValueSection from "@/components/ValueSection/ValueSection";
+import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       <ValueSection />
+      <Footer />
     </main>
   );
 }
