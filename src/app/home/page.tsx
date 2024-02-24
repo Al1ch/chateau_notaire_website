ab5330949ef11771dda67e4a32dc5cff49bc4da9
Alain Chea <alain.chea@efrei.net>
2024-02-24 21:22:10 +0100

    refactor(components): navbar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,14 @@
 import styles from "./page.module.scss";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import ServicesSection from "@/components/ServicesSection/ServicesSection";
+import ValueSection from "@/components/ValueSection/ValueSection";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <HeroSection />
       <ServicesSection />
+      <ValueSection />
     </main>
   );
 }
