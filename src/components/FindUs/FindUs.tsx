5bde6e8ee1191c330261e0bfaf3a39fbfc8ed296
Alain Chea <alain.chea@efrei.net>
2024-02-28 23:39:38 +0100

    fix(home): spacing

--- src/components/FindUs/FindUs.tsx
+++ src/components/FindUs/FindUs.tsx
@@ -9,8 +9,7 @@ const FindUs = () => {
         <iframe
           title="location"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46223.53321145887!2d1.4167236234408607!3d43.60714905225746!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1991371cf99eb3%3A0x1d693c043e612d3e!2sMe%20Paul-Emmanuel%20CHATEAU%20-%20NOTAIRE!5e0!3m2!1sfr!2sfr!4v1709083958840!5m2!1sfr!2sfr"
-          width="500"
-          height="300"
+          className={styles.map}
           allowFullScreen={false}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
