6002b01b8aa53525d5fcd2146294ed56d877fd05
Alain Chea <alain.chea@efrei.net>
2024-03-18 14:12:29 +0100

    feat(types): add type

--- types/model.d.ts
+++ types/model.d.ts
@@ -1,1 +1,17 @@
 export type SvgComponent =  React.FC<React.SVGProps<SVGSVGElement>>;
+
+
+export type Domain = {
+  title: string;
+  description: string;
+  id: number;
+  services: Services[];
+};
+
+
+type Services = {
+    title: string;
+    price: string;
+    id: number;
+  };
+  
