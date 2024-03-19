e3abcfcccb3e876b8a3596695b02a345c852a597
Alain Chea <alain.chea@efrei.net>
2024-03-19 14:25:18 +0100

    feat(components): drop down

---
+++ src/components/DropDownService/DropDownService.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useRef, useState } from "react";
+import styles from "./DropDownService.module.scss";
+import ChevronLogo from "@/assets/vectors/chevron.svg";
+import cn from "classnames";
+
+type Props = {
+  description: string;
+  title: string;
+};
+
+const DropDownService = ({ title, description }: Props) => {
+  const dropDownRef = useRef<HTMLDivElement>(null);
+  const [isClicked, setIsClicked] = useState(false);
+
+  return (
+    <button
+      onClick={() => setIsClicked(!isClicked)}
+      className={styles.container}
+    >
+      <div className={styles.titleContainer}>
+        <h1 className={styles.title}>{title}</h1>
+        <ChevronLogo
+          className={cn(styles.svg, { [styles.active]: isClicked })}
+        />
+      </div>
+      <p
+        className={styles.description}
+        style={
+          isClicked
+            ? { height: dropDownRef.current?.scrollHeight + "px" }
+            : { height: "0px" }
+        }
+        ref={dropDownRef}
+      >
+        {description}
+      </p>
+    </button>
+  );
+};
+
+export default DropDownService;
