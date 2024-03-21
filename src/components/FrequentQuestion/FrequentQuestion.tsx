bf99984ce1e1d134b3023dc25bbae320704fb28e
Alain Chea <alain.chea@efrei.net>
2024-03-21 15:14:55 +0100

    feat(components): question article

--- src/components/FrequentQuestion/FrequentQuestion.tsx
+++ src/components/FrequentQuestion/FrequentQuestion.tsx
@@ -1,76 +1,25 @@
 import React from "react";
 import styles from "./FrequentQuestion.module.scss";
 import Scintillant from "@/assets/vectors/scintillantCenter.svg";
+import { questions } from "@/data/question";
+import QuestionArticle from "../QuestionArticle/QuestionArticle";
 
 const FrequentQuestion = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.titleContainer}>
-          <h2 className={styles.title}>Frequently Asked Questions</h2>
+          <h2 className={styles.title}>Question Fréquentes</h2>
           <Scintillant />
         </div>
         <div className={styles.questionsList}>
-          <div className={styles.question}>
-            <h3
-              className={styles.questionTitle}
-            >{`What is the Notary's role?`}</h3>
-            <p className={styles.description}>
-              The notary is a public officer who receives a mandate from the
-              State to authenticate the contracts to which the parties wish to
-              confer
-            </p>
-          </div>
-          <div className={styles.question}>
-            <h3
-              className={styles.questionTitle}
-            >{`What is the Notary's role?`}</h3>
-            <p className={styles.description}>
-              The notary is a public officer who receives a mandate from the
-              State to authenticate the contracts to which the parties wish to
-              confer
-            </p>
-          </div>
-          <div className={styles.question}>
-            <h3
-              className={styles.questionTitle}
-            >{`What is the Notary's role?`}</h3>
-            <p className={styles.description}>
-              The notary is a public officer who receives a mandate from the
-              State to authenticate the contracts to which the parties wish to
-              confer
-            </p>
-          </div>
-          <div className={styles.question}>
-            <h3
-              className={styles.questionTitle}
-            >{`What is the Notary's role?`}</h3>
-            <p className={styles.description}>
-              The notary is a public officer who receives a mandate from the
-              State to authenticate the contracts to which the parties wish to
-              confer
-            </p>
-          </div>
-          <div className={styles.question}>
-            <h3
-              className={styles.questionTitle}
-            >{`What is the Notary's role?`}</h3>
-            <p className={styles.description}>
-              The notary is a public officer who receives a mandate from the
-              State to authenticate the contracts to which the parties wish to
-              confer
-            </p>
-          </div>
-          <div className={styles.question}>
-            <h3
-              className={styles.questionTitle}
-            >{`What is the Notary's role?`}</h3>
-            <p className={styles.description}>
-              The notary is a public officer who receives a mandate from the
-              State to authenticate the contracts to which the parties wish to
-              confer
-            </p>
-          </div>
+          {questions.map((question, index) => (
+            <QuestionArticle
+              key={index}
+              title={question.title}
+              description={question.description}
+            />
+          ))}
         </div>
       </div>
     </div>
