50d9049283441bd878ba18c040622e4b31c53fa2
Alain Chea <alain.chea@efrei.net>
2024-03-26 14:06:35 +0100

    refactor(content): website

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import styles from "./page.module.scss";
 import AboutHeroSection from "@/components/AboutHeroSection/AboutHeroSection";
 import OurStorySection from "@/components/OurStorySection/OurStorySection";
-import Footer from "@/components/Footer/Footer";
-import AboutValueSection from "@/components/AboutValuesSection/AboutValueSection";
 import FindUs from "@/components/FindUs/FindUs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Qui Sommes-Nous",
+  description:
+    "Maitrisant l'art de la légalité avec expertise, notre cabinet de notaire offre des services juridiques fiables et personnalisés. Protégez votre patrimoine, gérez vos successions et transactions immobilières en toute confiance avec nos notaires expérimentés. Contactez-nous dès aujourd'hui pour des conseils juridiques solides et des solutions sur mesure",
+};
 
 export default function About() {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@ import styles from "./page.module.scss";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import ServicesSection from "@/components/ServicesSection/ServicesSection";
 import ValueSection from "@/components/ValueSection/ValueSection";
-import StepSection from "@/components/StepSection/StepSection";
 import FindUs from "@/components/FindUs/FindUs";
 import NotaireRoleSection from "@/components/NotaireRoleSection/NotaireRoleSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Paul-Emmanuel Chateau - Notaire | Expertise Notariale",
+  description:
+    "Maitrisant l'art de la légalité avec expertise, notre cabinet de notaire offre des services juridiques fiables et personnalisés. Protégez votre patrimoine, gérez vos successions et transactions immobilières en toute confiance avec nos notaires expérimentés. Contactez-nous dès aujourd'hui pour des conseils juridiques solides et des solutions sur mesure",
+};
 
 export default function Home() {
   return (
@@ -12,7 +18,6 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       <NotaireRoleSection />
-      {/* <StepSection /> */}
       <ValueSection />
       <FindUs />
     </main>
